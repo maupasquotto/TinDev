d829c89548c3e419830402c85d1ea4c20de77c7c
Mauricio Pasquotto <maupasquotto@gmail.com>
2019-08-07 02:13:05 -0300

    - Backend Dev   - Add New Dev   - Like Dev   - Dislike Dev   - Match with Dev   - List unvisited Devs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect('mongodb+srv://<user>:<pass>@cluster0-jqolg.mongodb.net/<db>?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
