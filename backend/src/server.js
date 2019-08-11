2107a543094012ccad339a814cf30c33a7e62921
Mauricio Pasquotto <maupasquotto@gmail.com>
2019-08-10 22:19:34 -0300

    It's a march functon backend/frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,31 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://<user>:<pass>@cluster0-jqolg.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+    connectedUsers[user] = socket.id;
+});
+
+mongoose.connect('mongodb+srv://user_teste:0e4rAbKVPchUw33G@cluster0-jqolg.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+//mongoose.connect('mongodb+srv://<user>:<pass>@cluster0-jqolg.mongodb.net/omnistack8?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(( req, res, next ) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Main.css'
 
@@ -7,9 +8,12 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
+import { isContainer } from 'postcss-selector-parser';
 
 export default function Main({ match }) {
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadUsers() {
@@ -23,6 +27,17 @@ export default function Main({ match }) {
         }
 
         loadUsers();
+    }, [match.params.id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
+        });
+        
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        });
+
     }, [match.params.id]);
 
     async function handleLike(id) {
@@ -50,29 +65,41 @@ export default function Main({ match }) {
             <Link to='/'>
                 <img src={logo} alt="TinDev"/>
             </Link>
-                { users.length > 0 ? (
-                    <ul>
-                        {users.map(user => (
-                            <li key={user._id}>
-                                <img src={user.avatar} alt={user.name}/>
-                                <footer>
-                                    <strong>{user.name}</strong>
-                                    <p>{user.bio}</p>
-                                </footer>
-                                <div className="buttons">
-                                    <button type="button" onClick={() => handleDislike(user._id)}>
-                                        <img src={dislike} alt="dislike"/>
-                                    </button>
-                                    <button type="button" onClick={() => handleLike(user._id)}>
-                                        <img src={like} alt="like"/>
-                                    </button>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                )  : (
-                    <div className="empty">Acabou :(</div>
-                ) }
+            { users.length > 0 ? (
+                <ul>
+                    {users.map(user => (
+                        <li key={user._id}>
+                            <img src={user.avatar} alt={user.name}/>
+                            <footer>
+                                <strong>{user.name}</strong>
+                                <p>{user.bio}</p>
+                            </footer>
+                            <div className="buttons">
+                                <button type="button" onClick={() => handleDislike(user._id)}>
+                                    <img src={dislike} alt="dislike"/>
+                                </button>
+                                <button type="button" onClick={() => handleLike(user._id)}>
+                                    <img src={like} alt="like"/>
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )  : (
+                <div className="empty">Acabou :(</div>
+            ) }
+
+            { matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a Match" />
+
+                    <img className="avatar" src={matchDev.avatar} alt="Avatar"/>
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+
+                    <button type="button" onClick={() => setMatchDev(null)}>FECHAR</button>
+                </div>
+            )}
         </div>
     );
 };
